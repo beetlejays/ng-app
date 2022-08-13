1cc84378c34b2dd868a84570e6b61c2edc5f9ba7
Jay <me@aeiko.de>
2022-08-13 09:46:20 +0200

    Add success and alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ServerComponent } from './server/server.component';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { AlertComponent } from './alert/alert.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
