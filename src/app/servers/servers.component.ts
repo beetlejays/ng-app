fbe3261a48108f4517e20565c54553d07bb1645c
Jay <me@aeiko.de>
2022-08-14 09:17:57 +0200

    Add passing data to input value with event target

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = "no server created"
+  serverName = ""
   
   constructor() { 
     setTimeout( () => {
@@ -23,7 +24,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created"
-    console.log("works")
+   
+  }
+
+  onUpdateServerName(event: any) {
+this.serverName = event.target.value
   }
 
 }
