73d976690f1904da8dfa1b5de5914a5ed63f3633
Jay <me@aeiko.de>
2022-08-14 09:28:46 +0200

    Add button to output server name, using all 4 types of binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false
   serverCreationStatus = "no server created"
-  serverName = "tester"
+  serverName = ""
   
   constructor() { 
     setTimeout( () => {
@@ -23,7 +23,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created"
+    this.serverCreationStatus = "Server was created. Name is " + this.serverName
    
   }
 
